c14ebe9746cd613cc372b21f663027a22eb0c52c
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-12-22 22:15:19 +0700

    useState example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+// const orders = [100, 200, 300];
 
 function App() {
+  // const [counter, setCounter] = useState(() => {
+  //   const total = orders.reduce((total, cur) => total + cur);
+  //   return total;
+  // });
+
+  // const handleIncrease = () => {
+  //   setCounter(counter + 1);
+  // };
+
+  const [info, setInfo] = useState({
+    name: "Nguyen Van A",
+    age: 18,
+    address: "HCM",
+  });
+
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      bio: "Yêu màu hồng",
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>Increase</button>
     </div>
   );
 }
